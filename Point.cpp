5aab03417998c97e92d74980cce8af5dff58b2d6
Byron <byron.2.peretsky@ucdenver.edu>
2016-02-05 19:39:49 -0700

    Updated Point.cpp with all implementation functions

--- Point.cpp
+++ Point.cpp
@@ -1,4 +1,64 @@
-//
-// Created by Byron on 2/4/2016.
-//
+//Byron Peretsky
+//STUDENT ID: 105789918
+//Point class implementation file.
+//All functions for the pint class are here.
+
+#include "Point.h"
+#include <iostream>
+
+using namespace std;
+
+// Constructors
+
+// default constructor
+Point::Point(){
+
+    x = 0;
+    y = 0;
+    z = 0;
+
+}
+
+
+// Three-argument constructor
+Point::Point(double __x, double __y, double __z){
+
+    x = __x;
+    y = __y;
+    z = __z;
+
+}
+
+// Mutator methods
+void Point::setX(double newX){
+
+    x = newX;
+}
+
+void setY(double newY){
+
+    y = newY;
+}
+
+void setZ(double newZ){
+
+    z = newZ;
+}
+
+
+// Accessor methods
+double Point::getX() const{
+
+    return x;
+}
+
+double Point::getY() const{
+
+    return y:
+}
+
+double Point::getZ() const{
+
+    return z;
+}
 
